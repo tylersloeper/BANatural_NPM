1b39a3e63e58ee95913eb78b5684b8875d72aefa
Tyler Loeper <tloeper@jpay.com>
2020-02-11 12:03:11 -0500

    created DAL

---
+++ MyNPM/BaPortalNpm/DAL/DAL.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BaPortalNpm.DAL
+{
+    public class DAL
+    {
+        protected readonly BaNaturalNpmLocalEntities Entity = new BaNaturalNpmLocalEntities();
+
+        #region Seminars
+
+        public List<SeminarsNpm> SelectAllSeminars()
+        {
+            IQueryable<SeminarsNpm> seminarsSelected = from m in Entity.SeminarsNpms
+                select m;
+            List<SeminarsNpm> resultSet = seminarsSelected.ToList();
+            return resultSet;
+        }
+
+        public bool DeleteSeminarById(int id)
+        {
+            try
+            {
+                var seminarSelected = Entity.SeminarsNpms.First(m => m.Id == id);
+                Entity.SeminarsNpms.Remove(seminarSelected);
+                Entity.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool InsertNewSeminar(SeminarsNpm newSeminar)
+        {
+            try
+            {
+                Entity.SeminarsNpms.Add(newSeminar);
+                Entity.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        #endregion
+
+        #region Emails
+
+        public List<EmailNpm> SelectAllEmails()
+        {
+            IQueryable<EmailNpm> emailsSelected = from m in Entity.EmailNpms
+                select m;
+            List<EmailNpm> resultSet = emailsSelected.ToList();
+            return resultSet;
+        }
+
+        public bool DeleteEmailById(int id)
+        {
+            try
+            {
+                var emailsSelected = Entity.EmailNpms.First(m => m.Id == id);
+                Entity.EmailNpms.Remove(emailsSelected);
+                Entity.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool InsertNewEmail(EmailNpm newEmail)
+        {
+            try
+            {
+                Entity.EmailNpms.Add(newEmail);
+                Entity.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        #endregion
+    }
+}
