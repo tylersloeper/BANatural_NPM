37e49148d6310ecbd4fcab053256c9f56e71dd78
Tyler Loeper <tloeper@jpay.com>
2020-02-12 12:17:41 -0500

    all done, just need a delete button now

--- MyNPM/BaPortalNpm/Seminars.aspx
+++ MyNPM/BaPortalNpm/Seminars.aspx
@@ -6,44 +6,110 @@
         <a href="Treatments.aspx"><u>and more</u></a>. <br/>Sign up for a seminar today!
         <br /><br />
         <div style="padding: 10px;">
-            <h2><strong>2020:</strong></h2>
-            <div style="padding: 10px;">
-                <strong>January:</strong> <br/>
-                Coming soon...<br/><br/>
+            <asp:Panel id="ThisYearParentPanel" runat="server">
+                <h2><strong><asp:Label runat="server" Text="2020"/></strong></h2>
+                <asp:Panel style="padding: 15px;" runat="server" id="seminarsContainer">
+                    <asp:Panel id="LastMonth" runat="server">
+                        <h3><strong><asp:Label id="LastMonthTitle" runat="server" Text="January"/></strong></h3>
 
-                <strong>February:</strong> <br/>
-                Coming soon...<br/><br/>
+                        <asp:Panel id="LastMonthSlot1" style="padding: 15px;" Visible="False" runat="server">
+                            <strong><asp:Label id="LastMonthSlot1Title" runat="server" Text="Title"/></strong><br/>
+                            <asp:Label id="LastMonthSlot1When" runat="server" Text="Time: xxxx"/><br/>
+                            <asp:Label id="LastMonthSlot1Description" runat="server" Text=""/><br/><br/>
+                            <asp:Label id="LastMonthSlot1Where" runat="server" Text="Location: xxxxx"/><br/>
+                            <asp:Label id="LastMonthSlot1Price" runat="server" Text="Price: TBA"/><br/>
+                            <asp:Label id="LastMonthSlot1PhoneNumber" runat="server" Text="Contact us at: xxxxx"/><br/>
+                        </asp:Panel>
+                        <asp:Panel id="LastMonthSlot2" style="padding: 15px;" Visible="False" runat="server">
+                            <strong><asp:Label id="LastMonthSlot2Title" runat="server" Text="Title"/></strong><br/>
+                            <asp:Label id="LastMonthSlot2When" runat="server" Text="Time: xxxx"/><br/>
+                            <asp:Label id="LastMonthSlot2Description" runat="server" Text=""/><br/><br/>
+                            <asp:Label id="LastMonthSlot2Where" runat="server" Text="Location: xxxxx"/><br/>
+                            <asp:Label id="LastMonthSlot2Price" runat="server" Text="Price: TBA"/><br/>
+                            <asp:Label id="LastMonthSlot2PhoneNumber" runat="server" Text="Contact us at: xxxxx"/><br/>
+                        </asp:Panel>
+                    </asp:Panel>
+                    <asp:Panel id="ThisMonth" runat="server">
+                        <h3><strong><asp:Label id="ThisMonthTitle"  runat="server" Text=""/></strong></h3>
 
-                <strong>March:</strong> <br/>
-                Coming soon...<br/><br/>
+                        <asp:Panel id="ThisMonthSlot1" runat="server" Visible="False" style="padding: 15px;">
+                            <strong><asp:Label id="ThisMonthSlot1Title" runat="server" Text="Title"/></strong><br/>
+                            <asp:Label id="ThisMonthSlot1When" runat="server" Text="Time: xxxx"/><br/>
+                            <asp:Label id="ThisMonthSlot1Description" runat="server" Text=""/><br/><br/>
+                            <asp:Label id="ThisMonthSlot1Where" runat="server" Text="Location: xxxxx"/><br/>
+                            <asp:Label id="ThisMonthSlot1Price" runat="server" Text="Price: TBA"/><br/>
+                            <asp:Label id="ThisMonthSlot1PhoneNumber" runat="server" Text="Contact us at: xxxxx"/><br/>
+                        </asp:Panel>
+                        <asp:Panel id="ThisMonthSlot2" runat="server" style="padding: 15px;" Visible="False">
+                            <strong><asp:Label id="ThisMonthSlot2Title" runat="server" Text="Title"/></strong><br/>
+                            <asp:Label id="ThisMonthSlot2When" runat="server" Text="Time: xxxx"/><br/>
+                            <asp:Label id="ThisMonthSlot2Description" runat="server" Text=""/><br/><br/>
+                            <asp:Label id="ThisMonthSlot2Where" runat="server" Text="Location: xxxxx"/><br/>
+                            <asp:Label id="ThisMonthSlot2Price" runat="server" Text="Price: TBA"/><br/>
+                            <asp:Label id="ThisMonthSlot2PhoneNumber" runat="server" Text="Contact us at: xxxxx"/><br/>
+                        </asp:Panel>
+                    </asp:Panel>
+                    <asp:Panel id="NextMonth" runat="server">
+                        <h3><strong><asp:Label id="NextMonthTitle" runat="server" Text=""/></strong></h3>
 
-                <strong>April:</strong> <br/>
-                Coming soon...<br/><br/>
+                        <asp:Panel id="NextMonthSlot1" runat="server" Visible="False" style="padding: 15px;">
+                            <strong><asp:Label id="NextMonthSlot1Title" runat="server" Text="Title"/></strong><br/>
+                            <asp:Label id="NextMonthSlot1When" runat="server" Text="Time: xxxx"/><br/>
+                            <asp:Label id="NextMonthSlot1Description" runat="server" Text=""/><br/><br/>
+                            <asp:Label id="NextMonthSlot1Where" runat="server" Text="Location: xxxxx"/><br/>
+                            <asp:Label id="NextMonthSlot1Price" runat="server" Text="Price: TBA"/><br/>
+                            <asp:Label id="NextMonthSlot1PhoneNumber" runat="server" Text="Contact us at: xxxxx"/><br/>
+                        </asp:Panel>
+                        <asp:Panel id="NextMonthSlot2" runat="server" Visible="False" style="padding: 15px;">
+                            <strong><asp:Label id="NextMonthSlot2Title" runat="server" Text="Title"/></strong><br/>
+                            <asp:Label id="NextMonthSlot2When" runat="server" Text="Time: xxxx"/><br/>
+                            <asp:Label id="NextMonthSlot2Description" runat="server" Text=""/><br/><br/>
+                            <asp:Label id="NextMonthSlot2Where" runat="server" Text="Location: xxxxx"/><br/>
+                            <asp:Label id="NextMonthSlot2Price" runat="server" Text="Price: TBA"/><br/>
+                            <asp:Label id="NextMonthSlot2PhoneNumber" runat="server" Text="Contact us at: xxxxx"/><br/>
+                        </asp:Panel>
+                    </asp:Panel>
+                    <asp:Panel id="NextNextMonth" runat="server">
+                        <h3><strong><asp:Label id="NextNextMonthTitle" runat="server" Text=""/></strong></h3>
 
-                <strong>May:</strong> <br/>            
-                Coming soon...<br/><br/>
-
-                <strong>June:</strong> <br/>
-                Coming soon...<br/><br/>
-
-                <strong>July:</strong> <br/>
-                Coming soon...<br/><br/>
-
-                <strong>August:</strong> <br/>
-                Coming soon...<br/><br/>
-
-                <strong>September:</strong> <br/>
-                Coming soon...<br/><br/>
-
-                <strong>October:</strong> <br/>
-                Coming soon...<br/><br/>
-
-                <strong>November:</strong> <br/> 
-                Coming soon...<br/><br/>
-
-                <strong>December:</strong> <br/>
-                Coming soon...<br/><br/>
-            </div>
+                        <asp:Panel id="NextNextMonthSlot1" runat="server" Visible="False"  style="padding: 15px;">
+                            <strong><asp:Label id="NextNextMonthSlot1Title" runat="server" Text="Title"/></strong><br/>
+                            <asp:Label id="NextNextMonthSlot1When" runat="server" Text="Time: xxxx"/><br/>
+                            <asp:Label id="NextNextMonthSlot1Description" runat="server" Text=""/><br/><br/>
+                            <asp:Label id="NextNextMonthSlot1Where" runat="server" Text="Location: xxxxx"/><br/>
+                            <asp:Label id="NextNextMonthSlot1Price" runat="server" Text="Price: TBA"/><br/>
+                            <asp:Label id="NextNextMonthSlot1PhoneNumber" runat="server" Text="Contact us at: xxxxx"/><br/>
+                        </asp:Panel>
+                        <asp:Panel id="NextNextMonthSlot2" runat="server" Visible="False"  style="padding: 15px;">
+                            <strong><asp:Label id="NextNextMonthSlot2Title" runat="server" Text="Title"/></strong><br/>
+                            <asp:Label id="NextNextMonthSlot2When" runat="server" Text="Time: xxxx"/><br/>
+                            <asp:Label id="NextNextMonthSlot2Description" runat="server" Text=""/><br/><br/>
+                            <asp:Label id="NextNextMonthSlot2Where" runat="server" Text="Location: xxxxx"/><br/>
+                            <asp:Label id="NextNextMonthSlot2Price" runat="server" Text="Price: TBA"/><br/>
+                            <asp:Label id="NextNextMonthSlot2PhoneNumber" runat="server" Text="Contact us at: xxxxx"/><br/>
+                        </asp:Panel>
+                    </asp:Panel>
+                    <asp:Panel id="NextNextNextMonth" runat="server">
+                        <h3><strong><asp:Label id="NextNextNextMonthTitle" runat="server" Text=""/></strong></h3>
+                        <asp:Panel id="NextNextNextMonthSlot1" runat="server" style="padding: 15px;" Visible="False">
+                            <strong><asp:Label id="NextNextNextMonthSlot1Title" runat="server" Text="Title"/></strong><br/>
+                            <asp:Label id="NextNextNextMonthSlot1When" runat="server" Text="Time: xxxx"/><br/>
+                            <asp:Label id="NextNextNextMonthSlot1Description" runat="server" Text=""/><br/><br/>
+                            <asp:Label id="NextNextNextMonthSlot1Where" runat="server" Text="Location: xxxxx"/><br/>
+                            <asp:Label id="NextNextNextMonthSlot1Price" runat="server" Text="Price: TBA"/><br/>
+                            <asp:Label id="NextNextNextMonthSlot1PhoneNumber" runat="server" Text="Contact us at: xxxxx"/><br/>
+                        </asp:Panel>
+                        <asp:Panel id="NextNextNextMonthSlot2" runat="server" Visible="False" style="padding: 15px;">
+                            <strong><asp:Label id="NextNextNextMonthSlot2Title" runat="server" Text="Title"/></strong><br/>
+                            <asp:Label id="NextNextNextMonthSlot2When" runat="server" Text="Time: xxxx"/><br/>
+                            <asp:Label id="NextNextNextMonthSlot2Description" runat="server" Text=""/><br/><br/>
+                            <asp:Label id="NextNextNextMonthSlot2Where" runat="server" Text="Location: xxxxx"/><br/>
+                            <asp:Label id="NextNextNextMonthSlot2Price" runat="server" Text="Price: TBA"/><br/>
+                            <asp:Label id="NextNextNextMonthSlot2PhoneNumber" runat="server" Text="Contact us at: xxxxx"/><br/>
+                        </asp:Panel>
+                    </asp:Panel>
+                </asp:Panel>
+            </asp:Panel>
         </div>
     </div>
 </asp:Content>
--- MyNPM/BaPortalNpm/Seminars.aspx.cs
+++ MyNPM/BaPortalNpm/Seminars.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +10,248 @@ namespace BaPortalNpm.Sales
 {
     public partial class Seminars : System.Web.UI.Page
     {
+        // DAL
+        private readonly DAL.DAL _dataLayer = new DAL.DAL();
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            SetUpHeaders();
+            AssignSeminarToDate();
+        }
 
+        private void SetUpHeaders()
+        {
+            string LastMonth = DateTime.Now.AddMonths(-1).ToString("MMMM", CultureInfo.InvariantCulture);
+            string ThisMonth = DateTime.Now.AddMonths(0).ToString("MMMM", CultureInfo.InvariantCulture);
+            string NextMonth = DateTime.Now.AddMonths(1).ToString("MMMM", CultureInfo.InvariantCulture);
+            string NextNextMonth = DateTime.Now.AddMonths(2).ToString("MMMM", CultureInfo.InvariantCulture);
+            string NextNextNextMonth = DateTime.Now.AddMonths(3).ToString("MMMM", CultureInfo.InvariantCulture);
+
+            this.LastMonthTitle.Text = LastMonth;
+            this.ThisMonthTitle.Text = ThisMonth;
+            this.NextMonthTitle.Text = NextMonth;
+            this.NextNextMonthTitle.Text = NextNextMonth;
+            this.NextNextNextMonthTitle.Text = NextNextNextMonth;
+
+            if (DateTime.Now.AddMonths(-1).Year != DateTime.Now.Year)
+            {
+                this.LastMonth.Visible = false;
+            }
+
+            if (DateTime.Now.AddMonths(0).Year != DateTime.Now.Year)
+            {
+                // Not applicable.
+            }
+
+            if (DateTime.Now.AddMonths(1).Year != DateTime.Now.Year)
+            {
+                this.NextMonth.Visible = false;
+            }
+
+            if (DateTime.Now.AddMonths(2).Year != DateTime.Now.Year)
+            {
+                this.NextNextMonth.Visible = false;
+            }
+
+            if (DateTime.Now.AddMonths(3).Year != DateTime.Now.Year)
+            {
+                this.NextNextNextMonth.Visible = false;
+            }
+        }
+
+        private List<SeminarsNpm> GetSeminars()
+        {
+            return _dataLayer.SelectAllSeminars();
+        }
+
+        private void AssignSeminarToDate()
+        {
+            var allSeminars = GetSeminars();
+
+            foreach (var seminar in allSeminars)
+            {
+                DateTime currentSeminarDate = DateTime.Parse(seminar.InternalDate.ToString());
+
+                if (DateTime.Now.AddMonths(-1).Month == currentSeminarDate.Month)
+                {
+                    AssignSeminarToSlot(seminar, -1);
+                }
+                else if (DateTime.Now.AddMonths(0).Month == currentSeminarDate.Month)
+                {
+                    AssignSeminarToSlot(seminar, 0);
+                }
+                else if (DateTime.Now.AddMonths(1).Month == currentSeminarDate.Month)
+                {
+                    AssignSeminarToSlot(seminar, 1);
+                }
+                else if (DateTime.Now.AddMonths(2).Month == currentSeminarDate.Month)
+                {
+                    AssignSeminarToSlot(seminar, 2);
+                }
+                else if (DateTime.Now.AddMonths(3).Month == currentSeminarDate.Month)
+                {
+                    AssignSeminarToSlot(seminar, 3);
+                }
+            }
+        }
+
+        private void AssignSeminarToSlot(SeminarsNpm seminar, int month)
+        {
+            if (month == -1)
+            {
+                if (string.IsNullOrEmpty(LastMonthSlot1Description.Text))
+                {
+                    //available
+                    this.LastMonthSlot1.Visible = true;
+                    this.LastMonthSlot1Title.Text = seminar.Title;
+                    this.LastMonthSlot1When.Text = "Date:  " + seminar.When;
+                    this.LastMonthSlot1Description.Text = seminar.Description;
+                    this.LastMonthSlot1Where.Text = "Location: " + seminar.Where;
+                    this.LastMonthSlot1Price.Text = seminar.Price == 0f ? "Price: TBA" : "Price: " + seminar.Price + "$";
+                    this.LastMonthSlot1PhoneNumber.Text = "Contact us at: " + seminar.PhoneNumber;
+
+                }
+                else if (string.IsNullOrEmpty(LastMonthSlot2Description.Text))
+                {
+                    //available
+                    this.LastMonthSlot2.Visible = true;
+                    this.LastMonthSlot2Title.Text = seminar.Title;
+                    this.LastMonthSlot2When.Text = "Date:  " + seminar.When;
+                    this.LastMonthSlot2Description.Text = seminar.Description;
+                    this.LastMonthSlot2Where.Text = "Location: " + seminar.Where;
+                    this.LastMonthSlot2Price.Text = seminar.Price == 0f ? "Price: TBA" : "Price: " + seminar.Price + "$";
+                    this.LastMonthSlot2PhoneNumber.Text = "Contact us at: " + seminar.PhoneNumber;
+                }
+                else
+                {
+                    // None available
+                    return;
+                }
+            }
+            else if (month == 0)
+            {
+                if (string.IsNullOrEmpty(LastMonthSlot1Description.Text))
+                {
+                    //available
+                    this.ThisMonthSlot1.Visible = true;
+                    this.ThisMonthSlot1Title.Text = seminar.Title;
+                    this.ThisMonthSlot1When.Text = "Date:  " + seminar.When;
+                    this.ThisMonthSlot1Description.Text = seminar.Description;
+                    this.ThisMonthSlot1Where.Text = "Location: " + seminar.Where;
+                    this.ThisMonthSlot1Price.Text = seminar.Price == 0f ? "Price: TBA" : "Price: " + seminar.Price + "$";
+                    this.ThisMonthSlot1PhoneNumber.Text = "Contact us at: " + seminar.PhoneNumber;
+
+                }
+                else if (string.IsNullOrEmpty(ThisMonthSlot2Description.Text))
+                {
+                    //available
+                    this.ThisMonthSlot2.Visible = true;
+                    this.ThisMonthSlot2Title.Text = seminar.Title;
+                    this.ThisMonthSlot2When.Text = "Date:  " + seminar.When;
+                    this.ThisMonthSlot2Description.Text = seminar.Description;
+                    this.ThisMonthSlot2Where.Text = "Location: " + seminar.Where;
+                    this.ThisMonthSlot2Price.Text = seminar.Price == 0f ? "Price: TBA" : "Price: " + seminar.Price + "$";
+                    this.ThisMonthSlot2PhoneNumber.Text = "Contact us at: " + seminar.PhoneNumber;
+                }
+                else
+                {
+                    // None available
+                    return;
+                }
+            }
+            else if (month == 1)
+            {
+                if (string.IsNullOrEmpty(LastMonthSlot1Description.Text))
+                {
+                    //available
+                    this.NextMonthSlot1.Visible = true;
+                    this.NextMonthSlot1Title.Text = seminar.Title;
+                    this.NextMonthSlot1When.Text = "Date:  " + seminar.When;
+                    this.NextMonthSlot1Description.Text = seminar.Description;
+                    this.NextMonthSlot1Where.Text = "Location: " + seminar.Where;
+                    this.NextMonthSlot1Price.Text = seminar.Price == 0f ? "Price: TBA" : "Price: " + seminar.Price + "$";
+                    this.NextMonthSlot1PhoneNumber.Text = "Contact us at: " + seminar.PhoneNumber;
+
+                }
+                else if (string.IsNullOrEmpty(NextMonthSlot2Description.Text))
+                {
+                    //available
+                    this.NextMonthSlot2.Visible = true;
+                    this.NextMonthSlot2Title.Text = seminar.Title;
+                    this.NextMonthSlot2When.Text = "Date:  " + seminar.When;
+                    this.NextMonthSlot2Description.Text = seminar.Description;
+                    this.NextMonthSlot2Where.Text = "Location: " + seminar.Where;
+                    this.NextMonthSlot2Price.Text = seminar.Price == 0f ? "Price: TBA" : "Price: " + seminar.Price + "$";
+                    this.NextMonthSlot2PhoneNumber.Text = "Contact us at: " + seminar.PhoneNumber;
+                }
+                else
+                {
+                    // None available
+                    return;
+                }
+            }
+            else if (month == 2)
+            {
+                if (string.IsNullOrEmpty(LastMonthSlot1Description.Text))
+                {
+                    //available
+                    this.NextNextMonthSlot1.Visible = true;
+                    this.NextNextMonthSlot1Title.Text = seminar.Title;
+                    this.NextNextMonthSlot1When.Text = "Date:  " + seminar.When;
+                    this.NextNextMonthSlot1Description.Text = seminar.Description;
+                    this.NextNextMonthSlot1Where.Text = "Location: " + seminar.Where;
+                    this.NextNextMonthSlot1Price.Text = seminar.Price == 0f ? "Price: TBA" : "Price: " + seminar.Price + "$";
+                    this.NextNextMonthSlot1PhoneNumber.Text = "Contact us at: " + seminar.PhoneNumber;
+
+                }
+                else if (string.IsNullOrEmpty(NextNextMonthSlot2Description.Text))
+                {
+                    //available
+                    this.NextNextMonthSlot2.Visible = true;
+                    this.NextNextMonthSlot2Title.Text = seminar.Title;
+                    this.NextNextMonthSlot2When.Text = "Date:  " + seminar.When;
+                    this.NextNextMonthSlot2Description.Text = seminar.Description;
+                    this.NextNextMonthSlot2Where.Text = "Location: " + seminar.Where;
+                    this.NextNextMonthSlot2Price.Text = seminar.Price == 0f ? "Price: TBA" : "Price: " + seminar.Price + "$";
+                    this.NextNextMonthSlot2PhoneNumber.Text = "Contact us at: " + seminar.PhoneNumber;
+                }
+                else
+                {
+                    // None available
+                    return;
+                }
+            }
+            else if (month == 3)
+            {
+                if (string.IsNullOrEmpty(LastMonthSlot1Description.Text))
+                {
+                    //available
+                    this.NextNextNextMonthSlot1.Visible = true;
+                    this.NextNextNextMonthSlot1Title.Text = seminar.Title;
+                    this.NextNextNextMonthSlot1When.Text = "Date:  " + seminar.When;
+                    this.NextNextNextMonthSlot1Description.Text = seminar.Description;
+                    this.NextNextNextMonthSlot1Where.Text = "Location: " + seminar.Where;
+                    this.NextNextNextMonthSlot1Price.Text = seminar.Price == 0f ? "Price: TBA" : "Price: " + seminar.Price + "$";
+                    this.NextNextNextMonthSlot1PhoneNumber.Text = "Contact us at: " + seminar.PhoneNumber;
+
+                }
+                else if (string.IsNullOrEmpty(NextNextNextMonthSlot2Description.Text))
+                {
+                    //available
+                    this.NextNextNextMonthSlot2.Visible = true;
+                    this.NextNextNextMonthSlot2Title.Text = seminar.Title;
+                    this.NextNextNextMonthSlot2When.Text = "Date:  " + seminar.When;
+                    this.NextNextNextMonthSlot2Description.Text = seminar.Description;
+                    this.NextNextNextMonthSlot2Where.Text = "Location: " + seminar.Where;
+                    this.NextNextNextMonthSlot2Price.Text = seminar.Price == 0f ? "Price: TBA" : "Price: " + seminar.Price + "$";
+                    this.NextNextNextMonthSlot2PhoneNumber.Text = "Contact us at: " + seminar.PhoneNumber;
+                }
+                else
+                {
+                    // None available
+                    return;
+                }
+            }
         }
     }
 }
